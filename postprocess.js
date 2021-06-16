f44151e43b054cbafcb94bb75b8d65d56dd33e83
krb2162 <43657401+krb2162@users.noreply.github.com>
2021-06-16 13:02:05 -0700

    only import - test

--- postprocess.js
+++ postprocess.js
@@ -4,19 +4,19 @@
 // Has helper functions for manipulating csv, json, excel, zip, and image files
 import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.9/mod.ts' 
 
-// Step 1: Read the downloaded_filename JSON
-const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
-const json = await readJSON(filename)
-console.log(json)
+// // Step 1: Read the downloaded_filename JSON
+// const filename = Deno.args[0] // Same name as downloaded_filename `const filename = 'btc-price.json';`
+// const json = await readJSON(filename)
+// console.log(json)
 
-// Step 2: Filter specific data we want to keep and write to a new JSON file
-const jobs = Object.values(json.bpi); // convert property values into an array
-// const filteredjobs = jobss.map(rate => ({ 
-//     currency: rate.description,
-//     bitcoinRate: rate.rate
-// }));
-const filteredjobs = jobs.slice(1,7);
-// Step 3. Write a new JSON file with our filtered data
-const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
-await writeJSON(newFilename, filteredjobs) // create a new JSON file with just the Bitcoin price
-console.log("Wrote a post process file")
+// // Step 2: Filter specific data we want to keep and write to a new JSON file
+// const jobs = Object.values(json.bpi); // convert property values into an array
+// // const filteredjobs = jobss.map(rate => ({ 
+// //     currency: rate.description,
+// //     bitcoinRate: rate.rate
+// // }));
+// const filteredjobs = jobs.slice(1,7);
+// // Step 3. Write a new JSON file with our filtered data
+// const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
+// await writeJSON(newFilename, filteredjobs) // create a new JSON file with just the Bitcoin price
+// console.log("Wrote a post process file")
