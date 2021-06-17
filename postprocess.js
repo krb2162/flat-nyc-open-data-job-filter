fd47447ddf0be29a5ce92126d84b1d3d3f168c3e
krb2162 <43657401+krb2162@users.noreply.github.com>
2021-06-16 23:57:27 -0700

    Update postprocess.js

--- postprocess.js
+++ postprocess.js
@@ -19,7 +19,7 @@ const filteredjobs = jobs.slice(1,7);
 
 const filter2jobs = filteredjobs.map(joblocation => ({
       borough: borough,
-      Street: Street Name
+      Street: lot
 }));
 // // Step 3. Write a new JSON file with our filtered data
 const newFilename = `btc-price-postprocessed.json` // name of a new file to be saved
